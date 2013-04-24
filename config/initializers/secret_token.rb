5cf0d6e9e07da0cfc177411d2079c67e36f617b4
Travis Bybee <travis.bybee@gmail.com>
2013-04-24 13:14:23 -0600

    Inital commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeShare::Application.config.secret_token = 'f6c1a5e0affb245ad941ba69aa994c81630dc70b59dd04a9fcd5968efb61025be7fd7edad6988fa5ffe278cb1e6357deb546edcbcad52df71a36e559abe473c6'
